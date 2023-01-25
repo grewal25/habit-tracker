8d0f47d37af30cce3f93fb51cf9ba606a83bd551
Saranjeet Singh <saran@Saranjeets-MacBook-Pro.local>
2023-01-25 14:13:47 -0500

    hide button works

--- src/components/Gym/GymHome.tsx
+++ src/components/Gym/GymHome.tsx
@@ -1,31 +1,22 @@
-import { useState } from "react";
+import { useState } from "react"
 
-export default function GymHome() {
-  const days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
-  const [weeks, setWeeks] = useState<string>("");
 
-  const handleClick = (day: string) => {
-    setWeeks(day);
-  };
+export default function GymHome(){
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    const [weeks, setWeeks] = useState<string>('')
+    const [disable, setDisable] = useState<boolean>(false)
+    const handleClick=(day:string)=>{
+       setWeeks(day)
+       setDisable(!disable)
+    }
 
-  return (
-    <>
-      <h3>Gym home page</h3>
-      <p>select week:</p>
-      {days.map((day, idx) => (
-        <li key={idx}>
-          <button onClick={() => handleClick(day)}>{day}</button>
-        </li>
-      ))}
-      {<p>{weeks}</p>}
+    return<>
+    <h3>Gym home page</h3>
+    <p>select week:</p>
+    {days.map((day,idx) => <li key={idx}><button disabled={disable} onClick={()=>handleClick(day)}>{day}</button></li>)}
+    <br />
+       { <p>{weeks}</p>}
+  {disable && <p>change week: <button onClick={()=>setDisable(!disable)}>change week</button></p>}
+
     </>
-  );
 }
